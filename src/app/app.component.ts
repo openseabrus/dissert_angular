34fedbee6e079b463592edb993749b5494613b8c
l1904l <g.seabra@protonmail.com>
2018-08-07 11:20:38 +0100

    PureCSS, Menu installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [MenuComponent]
 })
 export class AppComponent {
-  title = 'gseabra';
+
+  menuOpened:boolean;
+
+  constructor() {
+    this.menuOpened = false;
+  }
+
+  toggleMenu(pos: boolean) {
+    this.menuOpened = pos;
+  }
 }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+
+  @Output() openMenu = new EventEmitter<boolean>();
+
+  selected: number;
+  menuItems: string[];
+  menuOpened: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.selected = 0;
+    this.menuItems = ["Adaptation Rules", "Triggers", "Actions"];
+    this.menuOpened = false;
+  }
+
+  public selectMenu(menuIndex: number) {
+    this.selected = menuIndex;
+  }
+
+  public getClasses(index: number) {
+    return {
+      "pure-menu-item": true,
+      "pure-menu-selected": this.selected == index
+    };
+  }
+
+  public toggleMenu(e: Event) {
+    e.preventDefault();
+    this.menuOpened = !this.menuOpened;
+    this.openMenu.next(this.menuOpened);
+  }
+
+}
