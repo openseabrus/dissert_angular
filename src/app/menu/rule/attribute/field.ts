4b3521b800358337949457b29b06e587e6ec4c66
l1904l <g.seabra@protonmail.com>
2018-11-01 19:12:06 +0000

    Now accepting multi-field triggers

--- src/app/menu/rule/attribute/field.ts
+++ src/app/menu/rule/attribute/field.ts
@@ -2,14 +2,21 @@ export class Field {
     name?: string;
     type: string;
     unit?: string;
+    value?: any;
 
-    constructor(name?: string, type?: string, unit?: string) {
+    constructor(name?: string, type?: string, unit?: string, value?: any) {
         this.name = name;
 
         if (type) {
             this.type = type;
         } else {
             this.type = 'number';
+        }
+
+        if (value) {
+            this.value = value;
+        }   else {
+            this.value = null;
         }
     }
 }
