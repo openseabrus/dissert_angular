6741deeabb19f54229e4a914a508310437f4657f
l1904l <g.seabra@protonmail.com>
2018-11-23 18:55:12 +0000

    New rule refactoring

--- src/app/menu/rule-list/rule-list.component.ts
+++ src/app/menu/rule-list/rule-list.component.ts
@@ -1,47 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 // import { Rule } from '../rule/rule';
-import { RuleService } from '../rule/rule-service.service';
 import { EntityService } from '../../services/entity/entity-service.service';
 import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-  query,
-  stagger
+	trigger,
+	state,
+	style,
+	animate,
+	transition,
+	query,
+	stagger
 } from '@angular/animations';
+import { GroupService } from 'src/app/services/group/group.service';
+import { Group } from '../rule/grouped/group';
 
 @Component({
-  selector: 'app-rule-list',
-  templateUrl: './rule-list.component.html',
-  styleUrls: ['./rule-list.component.css'],
-  animations: [
-	trigger('delete', [
-		transition(':leave', [
-			style({ height: '*' }),
-			animate(500, style({ transform: 'scale(0)' }))
+	selector: 'app-rule-list',
+	templateUrl: './rule-list.component.html',
+	styleUrls: ['./rule-list.component.css'],
+	animations: [
+		trigger('delete', [
+			transition(':leave', [
+				style({ height: '*' }),
+				animate(500, style({ transform: 'scale(0)' }))
+			])
 		])
-	])
-  ]
+	]
 })
 export class RuleListComponent implements OnInit {
 
-  rules: any[]; // Rule[];
-  ruleStates: boolean[];
+	rules: any[]; // Rule[];
+	ruleStates: boolean[];
 
-  constructor(private ruleService: RuleService, private entityService: EntityService) { }
+	groups: Group[];
 
-  ngOnInit() {
-	this.ruleService.getRules().subscribe(resp => {
-		console.log(resp);
-		this.rules = resp;
-		this.ruleStates = new Array(this.rules.length);
-		this.ruleStates.fill(true);
-	});
-  }
+	constructor(private groupService: GroupService, private entityService: EntityService) { }
 
-  deleteRule(index: number) {
+	ngOnInit() {
+		this.groupService.getRules().subscribe(resp => {
+			this.groups = resp;
+		});
+	}
+
+	deleteRule(groupId: number, attributeName: number, ruleId: number) {
+		this.groupService.deleteRule(groupId, attributeName, ruleId).subscribe(
+			success => {
+				console.log('Success');
+			},
+			error => console.log('errooor'));
+	}
+
+	/**
+	 * deleteRule(index: number) {
 	if (index >= 0 && index < this.rules.length) {
 		this.ruleService.deleteRule(this.rules[index]._id).subscribe(
 		success => { this.rules.splice(index, 1);
@@ -51,5 +60,6 @@ export class RuleListComponent implements OnInit {
 		error => console.log('errooor'));
 	}
   }
+	 */
 
 }
